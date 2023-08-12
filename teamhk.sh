607c440f6d78f98dffdbfbf04ad544b642ca68fe
tattookross <141897909+tattookross@users.noreply.github.com>
2023-08-12 02:42:46 -0500

    Update teamhk.sh

--- teamhk.sh
+++ teamhk.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 Black='\033[1;30m'        # Black 
- Red='\033[1;31m'          # Red 
- Green='\033[1;32m'        # Green 
- Yellow='\033[1;33m'       # Yellow 
- Blue='\033[1;34m'         # Blue 
- Purple='\033[1;35m'       # Purple 
- Cyan='\033[1;36m'         # Cyan 
- White='\033[1;37m'        # White 
- NC='\033[0m' 
- blue='\033[0;34m' 
- white='\033[0;37m' 
- lred='\033[0;31m' 
- brown='\033[0;33m' 
+Red='\033[1;31m'          # Red 
+Green='\033[1;32m'        # Green 
+Yellow='\033[1;33m'       # Yellow 
+Blue='\033[1;34m'         # Blue 
+Purple='\033[1;35m'       # Purple 
+Cyan='\033[1;36m'         # Cyan 
+White='\033[1;37m'        # White 
+NC='\033[0m' 
+blue='\033[0;34m' 
+white='\033[0;37m' 
+lred='\033[0;31m' 
+brown='\033[0;33m' 
   
- logo () { 
- echo -e "\033[1;31m \t 
+logo () { 
+echo -e "\033[1;31m \t 
 
 perform are:"           
 actions () {
